ca1ee1006da5420c7da84aa5810a0394f1c492cd
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-01-07 07:39:44 +0900

    ToDoList画面表示からAdditionalToDo画面への遷移まで実装

--- ToDoApp/AdditionalToDo/AdditionalToDoViewController.swift
+++ ToDoApp/AdditionalToDo/AdditionalToDoViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 final class AdditionalToDoViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    init() {
+        super.init(nibName: String(describing: AdditionalToDoViewController.self), bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
 }
--- ToDoApp/ToDoList/ToDoListViewController.swift
+++ ToDoApp/ToDoList/ToDoListViewController.swift
@@ -9,9 +9,44 @@ import UIKit
 
 final class ToDoListViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var additionalButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupNavigation()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        additionalButton.layer.cornerRadius = additionalButton.layer.frame.height / 2
+    }
+    
+    @IBAction func didTapAdditionalButton(_ sender: Any) {
+        let additionalToDoVC = AdditionalToDoViewController()
+        navigationController?.pushViewController(additionalToDoVC, animated: true)
+    }
+    
+    init() {
+        super.init(nibName: String(describing: ToDoListViewController.self), bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+// MARK: - setup
+extension ToDoListViewController {
+    
+    private func setupNavigation() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .systemBackground
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationItem.title = "ToDo"
+    }
+
 }
