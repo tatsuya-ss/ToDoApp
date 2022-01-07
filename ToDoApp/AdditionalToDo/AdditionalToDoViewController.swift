195aea443539d3f461f0a53a88c458c5046a24cd
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-01-07 11:03:03 +0900

    NavigationBarと追加ボタンの処理作成

--- ToDoApp/AdditionalToDo/AdditionalToDoViewController.swift
+++ ToDoApp/AdditionalToDo/AdditionalToDoViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 final class AdditionalToDoViewController: UIViewController {
     
+    private var additionalButton: UIBarButtonItem!
+    @IBOutlet private weak var AdditionaltextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupNavigation()
     }
     
     init() {
@@ -20,6 +23,35 @@ final class AdditionalToDoViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+// MARK: - setup
+extension AdditionalToDoViewController {
+    
+    private func setupNavigation() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .systemBackground
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationItem.title = "追加"
+        
+        additionalButton = UIBarButtonItem(title: "追加",
+                                           style: .done,
+                                           target: self,
+                                           action: #selector(didTapAdditionalButton))
+        navigationItem.rightBarButtonItem = additionalButton
+    }
+    
+}
+
+// MARK: - @objc
+extension AdditionalToDoViewController {
+    
+    @objc private func didTapAdditionalButton() {
+        navigationController?.popViewController(animated: true)
     }
     
 }
